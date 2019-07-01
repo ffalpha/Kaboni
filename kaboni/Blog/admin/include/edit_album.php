ba19d1c65e33a25494a81544810586ae9c5e343a
Kalana Mihiranga <kalanam217@gmail.com>
2019-07-02 02:16:38 +0530

    Admin panale updated

--- kaboni/Blog/admin/include/edit_album.php
+++ kaboni/Blog/admin/include/edit_album.php
@@ -1,16 +1,21 @@
 
+
+<hr style="  border: 3px solid green;
+           border-radius: 5px;">  
+<h1 style="color:green; text-align: center;">Edit album</h1><br>
+
 <?php
 
 
-      if(isset($_GET['name'])){
-          $name= $_GET['name'];
-           $query1="SELECT * FROM album where name='{$name}'";
-          $result1=mysqli_query($connect,$query1);
-           $row1=mysqli_fetch_assoc($result1);
-         
-       }else{
-          die("Error");
-      }
+if(isset($_GET['name'])){
+    $name= $_GET['name'];
+    $query1="SELECT * FROM album where name='{$name}'";
+    $result1=mysqli_query($connect,$query1);
+    $row1=mysqli_fetch_assoc($result1);
+
+}else{
+    die("Error");
+}
 
 
 ?>
@@ -18,10 +23,17 @@
 
 <div class="grid-containers">
     <div>
-         <div class="form-group">
+        <div class="form-group">
             <label for="title">Album Name</label>
             <input type="text" class="form-control" name="name" readonly value="<?php echo $row1['name']; $abname=$row1['name'];?> ">  
         </div> 
+        <form method='post' action='#' enctype='multipart/form-data'>
+            <div class="form-group">
+                <label for="title">Album Images</label>
+                <input type="file"  class="btn btn-primary"  name="imagefiles[]" multiple>  
+            </div>
+            <input type='submit' value='Add new pictures ' name='add' class="btn btn-success"> <br>
+        </form>
         <label for="table"><span><h3>Album</h3></span></label>
         <table class="table table-bordered table-hover">
             <thead class="thead-dark">
@@ -57,59 +69,136 @@ while($row=mysqli_fetch_assoc($result))
             </tbody>
         </table>
     </div>
-   
+
     <div>
         <form action="#" method="post">
-        <div class="form-group">
-            <label for="title">Authrozied user</label>
-            <input type="text" class="form-control" name="username"  value="<?php echo $row1['user']?>">  
-        </div> 
-        
-         <div class="form-group">
-            <label for="title">Privacy Level</label>
-              <select name="privacy" id="privacy"> 
-                   <?php if($row1['privacy']=="Public"){?>
+            <div class="form-group">
+                <label for="title">Authrozied user</label>
+                <input type="text" class="form-control" name="username"  value="<?php echo $row1['user']?>">  
+            </div> 
+
+            <div class="form-group">
+                <label for="title">Privacy Level</label>
+                <select name="privacy" id="privacy"> 
+                    <?php if($row1['privacy']=="Public"){?>
                     <option value='Public' selected>Public</option><option value='Private'>Private</option><?php }else {?>
                     <option value='Private' selected>Private</option><option value='Public' >Public</option><?php } ?>
-        </select>  
-        </div> 
+                </select>  
+            </div> 
             <input type='submit' value='Update' name='updatedetails' class="btn btn-success">    
-</form>
-    </div>  
+        </form>
 
+    </div>  
 
 </div>
 <!-- Updateing the album table-->
 <?php 
-    if (isset($_POST['updatedetails'])){
-       
-        $name=$abname;
-        $privacy=$_POST['privacy'];
-        $usermail=$_POST['username']; 
-        $query2="UPDATE album SET privacy='{$privacy}',user='{$usermail}' WHERE name='{$name}'";
-        $result2=mysqli_query($connect,$query2);
-        confirm($result2);
-        header('Location: ./album.php');
-  
-    }
+if (isset($_POST['updatedetails'])){
+
+    $name=$abname;
+    $privacy=$_POST['privacy'];
+    $usermail=$_POST['username']; 
+    $query2="UPDATE album SET privacy='{$privacy}',user='{$usermail}' WHERE name='{$name}'";
+    $result2=mysqli_query($connect,$query2);
+    confirm($result2);
+    header('Location: ./album.php');
+
+}
 if(isset($_GET['delete'])){
     $id=$_GET['delete'];
-    
-     $query="SELECT * FROM $abname where id={$id}";
-     $result=mysqli_query($connect,$query);
-     $row=mysqli_fetch_assoc($result);
+
+    $query="SELECT * FROM $abname where id={$id}";
+    $result=mysqli_query($connect,$query);
+    $row=mysqli_fetch_assoc($result);
     $image="./images/albums/$abname/{$row['imagename']}";
+    $image2="./images/albums/$abname/thumbnail/{$row['imagename']}";
     if(file_exists($image)){
         unlink($image);
     } else {
         die('file does not exist');
     }
-//    $query="Delete from posts where pid={$id}";
-//    $result=mysqli_query($connect,$query);
-//    header("Location:posts.php");
+    if(file_exists($image2)){
+        unlink($image2);
+    } else {
+        die('file does not exist');
+    }
+    $query="Delete from $abname where id={$id}";
+    $result=mysqli_query($connect,$query);
+    //     header("Location:./album.php?source=edit_album&name=$abname");
 }
 
 
+
+
+if(isset($_POST['add'])){
+
+    $id=$abname;
+    // Location 
+
+    $path = "./images/albums/".$id."/";
+    $thumbnail_path = "images/albums/".$id."/thumbnail/";
+    if (!file_exists($path)) {
+        createtable();
+        mkdir($path,755);
+
+
+    }if (!file_exists($thumbnail_path)) {
+        mkdir($thumbnail_path,755);
+    }
+
+    $name =strval($abname);
+    foreach($_FILES['imagefiles']['name'] as $key=>$val){
+        $filename = $_FILES['imagefiles']['name'][$key];
+
+        // Valid extension
+        $valid_ext = array('png','jpeg','jpg');
+
+
+        $location ="./images/albums/".$id."/".$filename;
+        $thumbnail_location = "images/albums/".$id."/thumbnail/".$filename;
+        // file extension
+        $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+        $file_extension = strtolower($file_extension);
+
+        // Check extension
+        if(in_array($file_extension,$valid_ext)){  
+
+            // Upload file
+            if(move_uploaded_file($_FILES['imagefiles']['tmp_name'][$key],$location)){
+
+                // Compress Image
+                compressImage($_FILES['imagefiles']['type'][$key],$location,$thumbnail_location,60);
+                $query="INSERT INTO {$name}(imagename,date) values('{$filename}',now())";
+                $result=mysqli_query($connect,$query);
+                confirm($result);
+
+            }else{
+                die("Error");
+            }
+
+        }
+    }
+
+    echo "Successfully Uploaded";
+}
+
+// Compress image
+function compressImage($type,$source, $destination, $quality) {
+
+    $info = getimagesize($source);
+
+    if ($type == 'image/jpeg') 
+        $image = imagecreatefromjpeg($source);
+
+    elseif ($type == 'image/gif') 
+        $image = imagecreatefromgif($source);
+
+    elseif ($type == 'image/png') 
+        $image = imagecreatefrompng($source);
+
+    imagejpeg($image, $destination, $quality);
+
+}
 
 ?>
 
